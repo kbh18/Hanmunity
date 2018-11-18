ee704df221cad3523a653863e2f918a4b1824922
Jinwoo <ypk02007@naver.com>
2018-11-18 22:53:23 +0900

    Update han.component.ts

--- han-master/myapp/src/app/components/han/han.component.ts
+++ han-master/myapp/src/app/components/han/han.component.ts
@@ -8,9 +8,21 @@ import { Component, OnInit } from '@angular/core';
 export class HanComponent implements OnInit {
   loginNum : number = 0;
   pageNum : number = 0;
-  boardNum : number = 0;
+  boardNum : number = 1;
+  samples : {
+    title : string,
+    date : string
+  }[];
+  
   constructor() { 
     console.log("Hanmunity is working...");
+    this.samples = [
+      {title: "장학금좀 팍팍 주세요", date: "2018/09/28(금)"},
+      {title: "흡연부스 늘려주세요", date: "2018/09/26(수)"},
+      {title: "상상관 에스컬레이터 설치", date: "2018/09/23(일)"},
+      {title: "아무튼 청원됨", date: "2018/09/22(토)"},
+      {title: "Hello, world!", date: "2018/09/20(목)"},
+    ];
   }
 
   ngOnInit() {
